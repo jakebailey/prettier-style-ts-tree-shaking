e5d7f8d50a341e8661013a5ad8cb67702228a8d4
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-10-18 10:58:47 -0700

    Hmm

--- run.sh
+++ run.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
-echo Creating baseline.min.js
-time npx terser baseline.js -o baseline.min.js --format=beautify --compress
-echo Creating typescript.min.js
-time npx terser typescript.js -o typescript.min.js --format=beautify --compress
+terser() {
+    npx terser $1 -o $2 --format=beautify --compress
+}
+
+rollup() {
+    npx rollup $1 --file $2 --format=cjs
+}
+
+echo Creating baseline
+terser baseline.js baseline.min.js
+rollup baseline.js baseline.rollup.js
+
+
+# echo Modifying typescript.js
+# cp baseline.js typescript.js
+
+# sed -i 's!generateDjb2Hash: \(\)!// \0!g' typescript.js
+# sed -i 's!TypeScriptServicesFactory: \(\)!// \0!g' typescript.js
+# sed -i 's!Version: \(\)!// \0!g' typescript.js
+# sed -i 's!transform: \(\)!// \0!g' typescript.js
+# sed -i 's!transform: \(\)!// \0!g' typescript.js
+
+echo Creating typescript
+terser typescript.js typescript.min.js
+rollup typescript.js typescript.rollup.js
